a65904b3312dbb195dfacd6140ecba73ae83365f
ABfry <is0605ek@ed.ritsumei.ac.jp>
2026-03-15 21:22:13 +0900

    feat: export RoomId from public API

--- src/index.ts
+++ src/index.ts
@@ -11,5 +11,8 @@ export type { Participant } from './domain/types/index.js'
 // Domain ports
 export type { ProviderAdapter } from './domain/ports/adapter.js'
 
+// Domain value objects
+export { RoomId } from './domain/value-objects/room-id.js'
+
 // Domain errors
 export { RoomNotFoundError, RoomExpiredError } from './domain/errors.js'
