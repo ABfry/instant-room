813e13344d0af33a3447bd6c9df05356b136805c
IH <88941921+ABfry@users.noreply.github.com>
2026-03-16 22:19:28 +0900

    feat: add Room.touch() method to reset TTL (#39)

--- tests/domain/entities/room.test.ts
+++ tests/domain/entities/room.test.ts
@@ -266,4 +266,12 @@ describe('Room', () => {
       expect(result).toBeInstanceOf(Promise)
     })
   })
+
+  describe('touch', () => {
+    it('resets the timer', () => {
+      const { room, timer } = createRoom()
+      room.touch()
+      expect(timer.reset).toHaveBeenCalledOnce()
+    })
+  })
 })
